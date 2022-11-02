1cf5729531691c972c703c3532b29c44183b5979
Meti Adugna <88801129+Meti-Adugna@users.noreply.github.com>
2022-11-02 19:07:32 +0300

    Update dominos.java

--- dominos.java
+++ dominos.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
         public static void main(String[] args) {
 
             Scanner sc  = new Scanner(System.in);
-            System.out.print("Enter the value of n: ");
             int n  = sc.nextInt();
-            System.out.print("Enter the value of m: ");
             int m = sc.nextInt();
             int dominos = m*n/2;
             System.out.println(dominos);
