a5cd686d4449774594a19a7c48fd906c67b0b0de
Meti Adugna <88801129+Meti-Adugna@users.noreply.github.com>
2022-11-03 16:45:54 +0300

    Create largestNumber.py

---
+++ largestNumber.py
@@ -0,0 +1,16 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        if not any(nums):
+            return "0"
+        class key:
+            def __init__(self,num):
+                self.num = num
+            def __lt__(self,other):
+                return self.num+other.num > other.num+self.num
+        heap = []
+        for i in nums:
+            heapq.heappush(heap,key(str(i)))
+        res = ""
+        while heap:
+            res += heapq.heappop(heap).num
+        return res
