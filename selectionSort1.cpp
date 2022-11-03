bc18493751f9d2cc9a4ec41e06aa67ad5ea369ea
Meti Adugna <88801129+Meti-Adugna@users.noreply.github.com>
2022-11-03 12:11:35 +0300

    Create selectionSort1.cpp

---
+++ selectionSort1.cpp
@@ -0,0 +1,27 @@
+class Solution{
+   public:
+    int select(int arr[], int i)
+    {
+        // code here such that selectionSort() sorts arr[]
+        
+    }
+    void selectionSort(int arr[], int n)
+    {
+       //code here
+       for(int i=0;i<n-1;i++)
+       {
+           int minindex=i;
+           for(int j=i+1;j<n;j++)
+           {
+               if(arr[j]<arr[minindex])
+               {
+                   minindex=j;
+               }
+           }
+           swap(arr[minindex],arr[i]);
+       }
+       
+    }
+};
+       
+       
