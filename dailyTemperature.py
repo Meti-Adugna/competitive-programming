3cce4b0e54ecfe36de9d67e70e9b3d94781035ec
Meti Adugna <88801129+Meti-Adugna@users.noreply.github.com>
2022-11-05 17:49:26 +0300

    Create dailyTemperature.py

---
+++ dailyTemperature.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        stack = []
+        ans = [0] * n
+        for i in range(n):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                ans[stack.pop()] = i - stack[-1]
+            stack.append(i)
+        return ans
